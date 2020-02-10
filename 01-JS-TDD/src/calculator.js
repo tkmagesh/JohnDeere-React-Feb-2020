0079b015587d58bddf93eaa6b814ae7a571846df
Magesh Kuppan <tkmagesh77@gmail.com>
2020-02-10 15:05:22 +0530

    calculator implementation

--- 01-JS-TDD/src/calculator.js
+++ 01-JS-TDD/src/calculator.js
@@ -1,3 +1,14 @@
-function add(x,y){
-    return x + y;
+function parseArg(n){
+    if (Array.isArray(n)) return add.apply(this, n);
+    if (typeof n === 'function') return parseArg(n());
+    return isNaN(n) ? 0 : parseInt(n);
 }
+function add(){
+    return arguments.length <= 1 ? parseArg(arguments[0]) : parseArg(arguments[0]) + add([].slice.call(arguments, 1));
+}
+
+//function invoked as a method of an obj
+//    this -> obj
+
+//function invoked as a function
+//     this -> global (window in the browser)
